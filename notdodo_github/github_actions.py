5b629a210bdd2a0708002001c0ab3f9824be836a
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2025-01-04 01:48:50 +0100

    enh: github action allowlist (#36)

---
+++ notdodo_github/github_actions.py
@@ -0,0 +1,12 @@
+"""
+Define the list of globally allowed GitHub Actions
+"""
+
+default_allowed_github_actions = [
+    "astral-sh/setup-uv@*",
+    "checkmarx/kics-github-action@*",
+    "notdodo/*",
+    "step-security/harden-runner@*",
+    "thollander/actions-comment-pull-request@*",
+    "pulumi/*",
+]
