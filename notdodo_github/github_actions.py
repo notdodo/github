f12296c7d0a1d0d8c30c334f8739ab8000adc41b
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2025-04-25 22:06:58 +0200

    Update github_actions.py
    Signed-off-by: Edoardo Rosa <6991986+notdodo@users.noreply.github.com>

--- notdodo_github/github_actions.py
+++ notdodo_github/github_actions.py
@@ -3,6 +3,7 @@ Define the list of globally allowed GitHub Actions
 """
 
 DEFAULT_ALLOWED_GITHUB_ACTIONS = [
+    "arduino/setup-task@*",
     "astral-sh/setup-uv@*",
     "checkmarx/kics-github-action@*",
     "gitleaks/gitleaks-action@*",
